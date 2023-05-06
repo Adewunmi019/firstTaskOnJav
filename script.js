b761b92baa2ab164047a530c33b110c51763d1f5
adewunmi019 <abuyemi.ope@gmail.com>
2023-05-06 08:55:15 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const mod = document.querySelector('.islam')
+const bod = document.querySelector('body')
+
+mod.addEventListener('click',()=>{
+    // document.querySelector('a').innerHTML='islam'>;
+    document.querySelector('body').classList.toggle('dark')
+});
